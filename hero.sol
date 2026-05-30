61dd29ba6737357535a3708432cf48ba373c160f
DinhCuong <159988337+dinhcuong261291@users.noreply.github.com>
2026-05-30 22:45:58 +0700

    hero.sol

---
+++ hero.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Hero {
+
+    string public name = "Hero";
+    string public symbol = "HERO";
+    uint256 public totalSupply = 1000000;
+
+}
